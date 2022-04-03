627067bf52cad20ee3a06c6b8199ca5b876b2029
Alexander Wiklund <alex@plexi.se>
2022-04-03 10:54:39 +0200

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "set-name":
+        return {
+          ...state,
+          name: action.payload.name,
+        };
+      case "set-price":
+        return {
+          ...state,
+          price: action.payload.price,
+        };
+      default:
+        throw new Error("Unknown action!");
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, {
+    id: "",
+    name: "",
+    type: "",
+    price: 1337,
+  });
+
+  dispatch({ type: "set-name", payload: { name: "Alex" } });
+  dispatch({ type: "set-price", payload: { price: 0 } });
+
+  const [data, setData] = useState({
+    id: "",
+    name: "",
+    type: "",
+    price: 1337,
+  });
+
+  setData((previous) => {
+    return {
+      ...previous,
+      name: "Hej",
+    };
+  });
+
+  return <div className="App"></div>;
 }
 
 export default App;
